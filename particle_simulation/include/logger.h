bf0fbe37a35337cb1f3d636dcbdbc05aa0492d60
Xertes0 <55015543+Xertes0@users.noreply.github.com>
2022-05-28 10:27:22 +0200

    First commit

---
+++ particle_simulation/include/logger.h
@@ -0,0 +1,20 @@
+#ifndef PS_LOGGER_H
+#define PS_LOGGER_H
+
+#include <stdio.h>
+
+#define _COLOR_NRM "\x1B[0m"
+#define _COLOR_RED "\x1B[31m"
+#define _COLOR_GRN "\x1B[32m"
+#define _COLOR_YEL "\x1B[33m"
+
+#define ERR(...) \
+	fprintf(stderr, _COLOR_RED"ERRO "_COLOR_NRM __VA_ARGS__ "\n");
+
+#define WARN(...) \
+	fprintf(stderr, _COLOR_YEL"WARN "_COLOR_NRM __VA_ARGS__ "\n");
+
+#define INFO(...) \
+	printf(_COLOR_GRN"INFO "_COLOR_NRM __VA_ARGS__ "\n");
+
+#endif
---
+++ particle_simulation/src/main.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include "logger.h"
+
+#define WWIDTH 800
+#define WHEIGHT 600
+
+int main()
+{
+	INFO("glfwInit");
+	if(glfwInit() != GLFW_TRUE) {
+		ERR("Failed to initialize glfw");
+		return EXIT_FAILURE;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	INFO("glfwCreateWindow");
+	GLFWwindow* window = glfwCreateWindow(WWIDTH, WHEIGHT, "Particle simulation", NULL, NULL);
+	if(window == NULL) {
+		ERR("Failed to create glfw window");
+		glfwTerminate();
+		return EXIT_FAILURE;
+	}
+	glfwMakeContextCurrent(window);
+
+	INFO("gladLoadGLLoader");
+	if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		ERR("Failed to initialize GLAD");
+		glfwTerminate();
+		return EXIT_FAILURE;
+	}
+
+	glViewport(0, 0, WWIDTH, WHEIGHT);
+
+	INFO("Main loop");
+	while(!glfwWindowShouldClose(window)) {
+		if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+			glfwSetWindowShouldClose(window, 1);
+		}
+
+		glClearColor(0.f, 0.f, 0.f, 1.f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	INFO("glfwTerminate");
+	glfwTerminate();
+	return EXIT_SUCCESS;
+}
